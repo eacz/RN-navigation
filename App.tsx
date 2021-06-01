135e7d50841233277a0feda480343d6001bc72e8
Esteban Canteros <estebanacanteros@gmail.com>
2021-06-01 11:31:16 -0300

    drawer basico

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
 import 'react-native-gesture-handler';
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
-import StackNavigator from './src/navigation/StackNavigator';
+//import StackNavigator from './src/navigation/StackNavigator';
 import { StatusBar } from 'react-native';
+import LateralMenu from './src/navigation/LateralMenu';
 
 const App = () => {
   return (
     <NavigationContainer >
       <StatusBar backgroundColor="#ffffff" barStyle="dark-content" />
-        <StackNavigator />
+        {/*<StackNavigator />*/}
+        <LateralMenu />
     </NavigationContainer>
   )
 }
