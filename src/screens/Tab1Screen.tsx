0556bdf5688bf1b65f822e8d14498627164e0cf2
Esteban Canteros <estebanacanteros@gmail.com>
2021-06-03 12:57:49 -0300

    icons (ionicons) installed

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import styles from '../themes/appTheme'
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Tab1Screen = () => {
   return (
-    <View>
-      <Text style={styles.title}>Tab1</Text>
+    <View style={styles.globalMargin}>
+      <Text style={styles.title}>Iconos</Text>
+      <Icon name="rocket" size={30} color='blue' />
     </View>
   )
 }
